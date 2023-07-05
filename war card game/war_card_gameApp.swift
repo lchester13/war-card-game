0ce91954be000141117ad75da3b4ebbd310e4752
lchester13 <leanna.r.chester@gmail.com>
2023-07-05 11:31:08 -0700

    Initial Commit

---
+++ war card game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+//
+//  Created by Leanna Chester on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
