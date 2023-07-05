300f39db74e59dde8ac615484f3ece275a9e2330
lchester13 <leanna.r.chester@gmail.com>
2023-07-05 12:10:30 -0700

    initial changes

--- war card game/ContentView.swift
+++ war card game/ContentView.swift
@@ -7,17 +7,50 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+        
+            VStack{
+                Spacer()
+                Image("logo").padding(.all)
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }.padding(.all)
+                Spacer()
+                Image("button").padding(.all)
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack {
+                        Text("Player").font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                        
+                    }
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                    
+                }.foregroundColor(.white)
+                Spacer()
+           
+            }
+        }}}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
